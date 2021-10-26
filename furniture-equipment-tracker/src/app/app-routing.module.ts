92ab75343c8a8755e395098578520dd50c97ec33
nikithams17 <nikithams17@gmail.com>
2021-10-26 11:42:24 +0530

    Initial commit

---
+++ furniture-equipment-tracker/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LoginsuccessComponent } from './loginsuccess/loginsuccess.component';
+import { RegistrationService } from './registration.service';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'loginsuccess',component:LoginsuccessComponent},
+  {path:'registration',component:RegistrationComponent},
+  {path:'login',component:LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ furniture-equipment-tracker/src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    emp_id!:number;
+    email!:string;
+    username!: string;
+    first_name!:string;
+    last_name!:string;
+    password!:string;
+    constructor(){}
+}
